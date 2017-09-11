1e3463e2a03d5aebd0c199e4018fd69150204897
lucaswantz <lucaswantz@hotmail.com>
2017-09-11 12:14:23 -0300

    Teste Issue 1
    Primeiro teste de commit

--- Projeto Lucas WEB/LUCAS/PROJETO WEB/TeamManager/src/java/entidades/Informacoes.java
+++ Projeto Lucas WEB/LUCAS/PROJETO WEB/TeamManager/src/java/entidades/Informacoes.java
@@ -22,20 +22,20 @@ public class Informacoes {
 
         int retorno = 0;
 
-//        try {
-//
-//            String sql = "SELECT count(*) from jogo "
-//                    + "where jogo.pontos_time > jogo.pontos_adversario "
-//                    + " and jogo.status = 'F' ";
-//
-//            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
-//
-//            resul.next();
-//            retorno = resul.getInt(1);
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+
+            String sql = "SELECT count(*) from jogo "
+                    + "where jogo.pontos_time > jogo.pontos_adversario "
+                    + " and jogo.status = 'F' ";
+
+            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
+
+            resul.next();
+            retorno = resul.getInt(1);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         return retorno;
 
@@ -44,20 +44,20 @@ public class Informacoes {
     public int getEmpates() {
         int retorno = 0;
 
-//        try {
-//
-//            String sql = "SELECT count(*) from jogo "
-//                    + "where jogo.pontos_time = jogo.pontos_adversario "
-//                    + " and jogo.status = 'F' ";
-//
-//            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
-//
-//            resul.next();
-//            retorno = resul.getInt(1);
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+
+            String sql = "SELECT count(*) from jogo "
+                    + "where jogo.pontos_time = jogo.pontos_adversario "
+                    + " and jogo.status = 'F' ";
+
+            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
+
+            resul.next();
+            retorno = resul.getInt(1);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         return retorno;
     }
@@ -65,20 +65,20 @@ public class Informacoes {
     public int getDerrotas() {
         int retorno = 0;
 
-//        try {
-//
-//            String sql = "SELECT count(*) from jogo "
-//                    + "where jogo.pontos_time < jogo.pontos_adversario "
-//                    + " and jogo.status = 'F' ";
-//
-//            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
-//
-//            resul.next();
-//            retorno = resul.getInt(1);
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+
+            String sql = "SELECT count(*) from jogo "
+                    + "where jogo.pontos_time < jogo.pontos_adversario "
+                    + " and jogo.status = 'F' ";
+
+            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
+
+            resul.next();
+            retorno = resul.getInt(1);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         return retorno;
     }
@@ -86,44 +86,44 @@ public class Informacoes {
     public int getJogadores() {
         int retorno = 0;
 
-//        try {
-//
-//            String sql = "SELECT count(*) from jogador ";
-//
-//            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
-//
-//            resul.next();
-//            retorno = resul.getInt(1);
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+
+            String sql = "SELECT count(*) from jogador ";
+
+            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
+
+            resul.next();
+            retorno = resul.getInt(1);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         return retorno;
     }
 
     public Jogo getJogo() {
-        Jogo ultimojogo = new Jogo();
+        Jogo ultimojogo;
         int retorno = 0;
-//
-//        try {
-//
-//            String sql = "Select codigo "
-//                    + "from jogo "
-//                    + "where jogo.status = 'F' "
-//                    + "order by data desc "
-//                    + "limit 1 ";
-//
-//            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
-//
-//            resul.next();
-//            retorno = resul.getInt(1);
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        
-//        ultimojogo = (Jogo) new JogoDAO().consultarId(retorno);
+
+        try {
+
+            String sql = "Select codigo "
+                    + "from jogo "
+                    + "where jogo.status = 'F' "
+                    + "order by data desc "
+                    + "limit 1 ";
+
+            ResultSet resul = ConexaoBD.getInstance().getConnection().createStatement().executeQuery(sql);
+
+            resul.next();
+            retorno = resul.getInt(1);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Informacoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ultimojogo = (Jogo) new JogoDAO().consultarId(retorno);
         
         return ultimojogo;
 
